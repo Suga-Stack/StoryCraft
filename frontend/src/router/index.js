391482a71669303a1540f92cf0069f3963951f7b
pengqian23 <1205474645@qq.com>
2025-10-30 10:25:06 +0800

    Revert "Merge branch 'pq/app-frontend' into gametest"
    This reverts commit f57ce435829ee709b437ee6fcdc13d075644890d, reversing
    changes made to 8faedcf5d49696cf8ad9647509b25137a94e9b91.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,16 +4,6 @@ import GameIntroduction from '../views/game_introduction.vue'
 import GamePage from '../views/GamePage.vue'
 import SettlementPage from '../views/SettlementPage.vue'
 import CreateWork from '../views/CreateWork.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from '../views/LoginPage.vue'
-import RegisterPage from '../views/RegisterPage.vue'
-import PreferencesPage from '../views/PreferencesPage.vue'
-//import BookshelfPage from '../views/BookshelfPage.vue'
-//import SearchPage from '../views/SearchPage.vue'
-//import BookstorePage from '../views/BookstorePage.vue'
-//import CreatePage from '../views/CreatePage.vue'
-//import ProfilePage from '../views/ProfilePage.vue'
-//import MyCreationsPage from '../views/MyCreationsPage.vue'
 
 const routes = [
   {
@@ -40,21 +30,6 @@ const routes = [
     path: '/settlement',
     name: 'Settlement',
     component: SettlementPage
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: LoginPage
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: RegisterPage
-  },
-  {
-    path: '/preferences',
-    name: 'preferences',
-    component: PreferencesPage
   }
 ]
 
