1f041d8567f5242691e3efe761588d70cad5e0aa
ssr <2728992203@qq.com>
2025-11-19 13:21:47 +0800

    ✨ feat(作品): 完善后台管理，增加取消发布方法

--- backend/gameworks/urls.py
+++ backend/gameworks/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('gameworks/<int:pk>/', GameworkViewSet.as_view({'get': 'retrieve'}), name='gamework-detail'),
     path('publish/<int:pk>/', PublishGameworkViewSet.as_view({'post': 'publish'}), name='publish-gamework'),
+    path('unpublish/<int:pk>/', PublishGameworkViewSet.as_view({'post': 'unpublish'}), name='unpublish-gamework'),
     path('search/', GameworkSearchView.as_view(), name='gamework-search'),
     path("recommend/", RecommendView.as_view(), name="gamework-recommend"),
     path('favorite-leaderboard/', GameworkFavoriteLeaderboardViewSet.as_view({'get': 'list'}), name='gamework-leaderboard'),
