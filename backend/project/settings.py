b8e4d82c1b0b5ef1cc7e366d7ef309c389906e5c
ssr <2728992203@qq.com>
2025-11-20 00:37:18 +0800

    📃 docs(软件页面): 完善软件页面API文档

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt', 
+    'drf_spectacular',
     'drf_yasg',
     'users',  # 用户
     'gameworks',  # 游戏作品
@@ -155,6 +156,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 from datetime import timedelta
